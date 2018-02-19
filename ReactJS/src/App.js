7a7c9a3283ea87248429ff882d4b438c0498e56a
Vipul <vipulrawat.imi@live.in>
2018-02-19 21:57:51 +0530

    Update App.js

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -6,6 +6,7 @@ import Grid from 'material-ui/Grid';
 
 class App extends Component {
   render() {
+    return(
       <div>
         <Grid container spacing={24}>
               <Grid item xs={12}>
